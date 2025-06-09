1519791592bdb6868c913f80f52d2447b7cb33b3
rapture-mc <145347290+rapture-mc@users.noreply.github.com>
2025-06-09 22:20:22 +0930

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,37 +2,37 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     megacorp = {
       url = "github:rapture-mc/nixos-module";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    terranix = {
+      url = "github:terranix/terranix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs: let
-    vars = import ./vars;
-  in
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-
-      src = ./.;
-
-      snowfall = {
-        namespace = "megacorp";
-      };
-
-      systems.modules.nixos = with inputs; [
+  outputs = { self, nixpkgs, megacorp, terranix, ... }: let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.nixos-dev = nixpkgs.lib.nixosSystem {
+      modules = [
         megacorp.nixosModules.default
+        self.nixosModules.default
+        ./configuration.nix
+        ./hardware-configuration.nix
       ];
-
-      systems.hosts.nixos-dev.specialArgs = {
-        vars = vars;
-      };
     };
+
+    nixosModules.default = { config, ... }: {
+      imports = [
+        (import ./modules {inherit config terranix lib pkgs system;})
+      ];
+    };
+  };
 }
