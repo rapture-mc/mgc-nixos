2a2fa130ce281483a5d807891bc69592ec681681
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-11 18:50:58 +0930

    updating RST01

--- machines/servers/MGC-DRW-RST01/default.nix
+++ machines/servers/MGC-DRW-RST01/default.nix
@@ -1,16 +1,18 @@
 {
   nixpkgs,
-  megacorp,
+  self,
   vars,
   ...
 }:
 nixpkgs.lib.nixosSystem {
   modules = [
-    megacorp.nixosModules.default
+    self.nixosModules.default
     {
       imports = [
         ../../qemu-hardware-config.nix
-        (import ../../base-config.nix {inherit vars;})
+        (import ../../base-config.nix {
+          inherit vars;
+        })
       ];
 
       networking.hostName = "MGC-DRW-RST01";
@@ -39,7 +41,7 @@ nixpkgs.lib.nixosSystem {
         services = {
           comin = {
             enable = true;
-            repo = "https://github.com/rapture-mc/mgc-machines";
+            repo = "https://github.com/rapture-mc/mgc-nixos";
           };
 
           restic.sftp-server = {
@@ -49,7 +51,7 @@ nixpkgs.lib.nixosSystem {
           };
         };
 
-        virtualisation.qemu-guest.enable = true;
+        virtualisation.libvirt.guest.enable = true;
       };
     }
   ];
