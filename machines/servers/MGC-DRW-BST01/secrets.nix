dacaa58fcec841049605ff95f27ddc68b20b9cfc
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-14 12:49:35 +0930

    updating sops path and removing PWS from sops

--- machines/servers/MGC-DRW-BST01/secrets.nix
+++ machines/servers/MGC-DRW-BST01/secrets.nix
@@ -1,6 +1,6 @@
 {vars}: {
   sops = {
-    defaultSopsFile = ../../../sops/secrets/default.yaml;
+    defaultSopsFile = ../../../sops/default.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${vars.adminUser}/.config/sops/age/keys.txt";
     secrets.restic-repo-password = {};
