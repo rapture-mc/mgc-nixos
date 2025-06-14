cb30530592d37942f2e0bee67d107b366f0a1aa4
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-14 11:52:51 +0930

    making alphabetical

--- machines/default.nix
+++ machines/default.nix
@@ -1,11 +1,11 @@
 {importMachineConfig, ...}: {
   # Servers
   MGC-DRW-BST01 = importMachineConfig "servers" "MGC-DRW-BST01";
+  MGC-DRW-DGW01 = importMachineConfig "servers" "MGC-DRW-DGW01";
   MGC-DRW-DMC01 = importMachineConfig "servers" "MGC-DRW-DMC01";
+  MGC-DRW-FBR01 = importMachineConfig "servers" "MGC-DRW-FBR01";
   MGC-DRW-RST01 = importMachineConfig "servers" "MGC-DRW-RST01";
   MGC-DRW-RVP01 = importMachineConfig "servers" "MGC-DRW-RVP01";
-  MGC-DRW-DGW01 = importMachineConfig "servers" "MGC-DRW-DGW01";
-  MGC-DRW-FBR01 = importMachineConfig "servers" "MGC-DRW-FBR01";
   MGC-DRW-VLT01 = importMachineConfig "servers" "MGC-DRW-VLT01";
 
   # Hypervisors
