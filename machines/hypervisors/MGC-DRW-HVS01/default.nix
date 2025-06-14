64efbf084c50699947f14b7982246ced6747f9f4
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-14 21:41:48 +0930

    triggering comin

--- machines/hypervisors/MGC-DRW-HVS01/default.nix
+++ machines/hypervisors/MGC-DRW-HVS01/default.nix
@@ -79,7 +79,7 @@ nixpkgs.lib.nixosSystem {
                 vm_hostname_prefix = "MGC-DRW-TMS";
                 os_img_url = "/var/lib/libvirt/images/packer-win2022.qcow2";
                 memory = "8192";
-                vcpu = 4;
+                vcpu = 3;
                 autostart = false;
                 running = false;
               };
