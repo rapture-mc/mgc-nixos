caa44c8ec862eb45ca94f413db6d0b11528b7256
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-15 15:59:19 +0930

    Revert "adding to HVS01"
    This reverts commit 596185adcb0a5edaccd0e314e674db3585bef9df.

--- machines/hypervisors/MGC-DRW-HVS01/default.nix
+++ machines/hypervisors/MGC-DRW-HVS01/default.nix
@@ -24,33 +24,6 @@ nixpkgs.lib.nixosSystem {
 
       systemd.watchdog.rebootTime = "15s";
 
-      security.pki.certificates = [
-        ''
-        MGC-DRW-VLT01 Root Certificate
-        ==============================
-        -----BEGIN CERTIFICATE-----
-        MIIDTzCCAjegAwIBAgIUIOB3FtWwJB6HUgbGaOmXhL5U0wMwDQYJKoZIhvcNAQEL
-        BQAwHjEcMBoGA1UEAxMTbWVnYWNvcnAuaW5kdXN0cmllczAeFw0yNTA2MTQxMDMz
-        NTNaFw0zNTA2MTIxMDM0MjNaMB4xHDAaBgNVBAMTE21lZ2Fjb3JwLmluZHVzdHJp
-        ZXMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDTzfNaQsJt6ahmaOoO
-        PHzjBVSYw2NLcge3Li5JDhk/xoOexSf4YrIRSDFgHJUBpxy+9iXiz28RSooqcgQv
-        NWhMG1iwEAGBiMte7AjspSobk4vYWcWVT2jb6waZ78oo2Odp9lgeDBuAN5EhwqNh
-        R7cbaM8drZxiuGAqW1YLnv2pIJYZ1w92QfYHc10+6ITaWHoj3lEtyQJghKZMHA9H
-        vJ5SUtKsajY9ZO4rKKyKKOmVD8h0rV5ymCQC/XDlDT2ZWCoe8NqNAk2TIcTpQ+a9
-        8oShY/QMMP85/bgvutnHE8Gpj62n7WZXAel4YjCT5hxzJBOY1olgwrvepNzjM0+5
-        W5oZAgMBAAGjgYQwgYEwDgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8w
-        HQYDVR0OBBYEFMlFeVt1Ha+Vd+Irl5FtBYrgVhHdMB8GA1UdIwQYMBaAFMlFeVt1
-        Ha+Vd+Irl5FtBYrgVhHdMB4GA1UdEQQXMBWCE21lZ2Fjb3JwLmluZHVzdHJpZXMw
-        DQYJKoZIhvcNAQELBQADggEBALglBMrVv3ty6ViyTGNvOa7wo1P5lLPF4ohv2dfp
-        Id+yyRI1p2G+T9VAYsxXoCtpG3rg+msFb+3eyffUJlozSf9OqxOdIEZB813sirin
-        ol+NddmO9i6VMJiBM5PPZEWO2aIA02xQUs2DhoyUY66zSu+NGndw6NNsiqZuMj+j
-        etazpTl6sUOcF79+WyUgNRFQEL2Lqhw0XGO9j5hwZCFjmYbaxSfG06i2Ozqjazgb
-        mouwe3PEK3k4TqcfihsdGnhj7RYjE22cLaAjO0BnqtVKK7+7zgtswKAfuEm3S3DZ
-        M++BDsTUu47+h7ly/lRaK0DkMNtNIVwCI4r9feY8Zh20d78=
-        -----END CERTIFICATE-----
-        ''
-      ];
-
       megacorp = {
         config = {
           bootloader = {
