4d3797a0cacfb977b6ffd56a66b0b4230d77d60d
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-15 12:54:11 +0930

    fixing internalDomain reference

--- machines/servers/MGC-DRW-VLT01/vault/root-cert-2025.nix
+++ machines/servers/MGC-DRW-VLT01/vault/root-cert-2025.nix
@@ -2,7 +2,7 @@
   root-cert-2025 = {
     backend     = "\${ vault_mount.pki.path }";
     type        = "internal";
-    common_name = vars.internalDomain;
+    common_name = vars.networking.internalDomain;
     ttl         = 315360000;
     issuer_name = "root-2025";
   };
