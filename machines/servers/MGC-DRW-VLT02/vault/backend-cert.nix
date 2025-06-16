16145e5baad0512dbde6734b54e2ad9d076f4976
Ben Harris <ben.r.harris@protonmail.com>
2025-06-16 19:40:28 +0930

    re-triggering

--- machines/servers/MGC-DRW-VLT02/vault/backend-cert.nix
+++ machines/servers/MGC-DRW-VLT02/vault/backend-cert.nix
@@ -4,7 +4,7 @@
     backend     = "\${ vault_pki_secret_backend_role.intermediate-role.backend }";
     name        = "\${ vault_pki_secret_backend_role.intermediate-role.name }";
     common_name = "vault02.${vars.networking.internalDomain}";
-    ttl         = 31536000;  # 1 year
+    ttl         = 3153600;  # 1 year
     revoke      = true;
   };
 }
