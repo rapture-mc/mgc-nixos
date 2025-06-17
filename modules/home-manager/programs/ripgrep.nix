15f3abde51ffb5b66a0ceac4e84402be65239d9e
Ben Harris <ben.r.harris@protonmail.com>
2025-06-17 20:44:01 +0930

    nvm cant introdue option here

--- modules/home-manager/programs/ripgrep.nix
+++ modules/home-manager/programs/ripgrep.nix
@@ -1,15 +1,5 @@
 {osConfig, lib, ...}: let
   cfg = osConfig.megacorp;
-
-  inherit (lib)
-    mkIf
-    mkEnableOption;
-in {
-  option.megacorp.programs.ripgrep = {
-    enable = mkEnableOption "Enable ripgrep";
-  };
-
-  config = mkIf (cfg.programs.nixvim.enable || cfg.programs.ripgrep.enable) {
-    programs.fastfetch.enable = true;
-  };
+in lib.mkIf cfg.programs.nixvim.enable {
+  programs.fastfetch.enable = true;
 }
