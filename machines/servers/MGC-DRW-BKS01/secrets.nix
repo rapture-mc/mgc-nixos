86257f000a42cf9519d65699c1d53ddb45146100
Ben Harris <ben.r.harris@protonmail.com>
2025-06-20 10:28:32 +0930

    renaming secret

--- machines/servers/MGC-DRW-BKS01/secrets.nix
+++ machines/servers/MGC-DRW-BKS01/secrets.nix
@@ -3,6 +3,6 @@
     defaultSopsFile = ../../../sops/default.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${vars.adminUser}/.config/sops/age/keys.txt";
-    secrets.vault-root-token = {};
+    secrets.bookstack-keyfile = {};
   };
 }
