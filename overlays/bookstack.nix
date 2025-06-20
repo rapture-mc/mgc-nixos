094784753a79717c65101cbb7c148bb10fa0e73c
Ben Harris <ben.r.harris@protonmail.com>
2025-06-20 16:06:30 +0930

    adding comment

--- overlays/bookstack.nix
+++ overlays/bookstack.nix
@@ -1,3 +1,5 @@
-{ nixpkgs2411 }: final: prev: {
-  bookstack = nixpkgs2411.legacyPackages.${final.system}.bookstack;
+# bookstack-setup.service in 24.05 errors out with "doctrine deprecation not found"
+# Here we use the 24.11 version of bookstack instead
+{nixpkgs24-11}: final: prev: {
+  bookstack = nixpkgs24-11.legacyPackages.${final.system}.bookstack;
 }
