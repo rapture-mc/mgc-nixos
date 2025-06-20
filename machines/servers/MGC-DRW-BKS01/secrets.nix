003dd8c5be19cd286d23e6e2960609ba09082d3e
Ben Harris <ben.r.harris@protonmail.com>
2025-06-20 10:32:53 +0930

    adding owner/group to secret

--- machines/servers/MGC-DRW-BKS01/secrets.nix
+++ machines/servers/MGC-DRW-BKS01/secrets.nix
@@ -3,6 +3,9 @@
     defaultSopsFile = ../../../sops/default.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${vars.adminUser}/.config/sops/age/keys.txt";
-    secrets.bookstack-keyfile = {};
+    secrets.bookstack-keyfile = {
+      owner = "bookstack";
+      group = "bookstack";
+    };
   };
 }
