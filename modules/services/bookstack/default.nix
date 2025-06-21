7f83392a9ebedfabf83b4cfe15636e457cd5cfab
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-06-21 12:27:32 +0930

    moving nginx option to a _shared dir

--- modules/services/bookstack/default.nix
+++ modules/services/bookstack/default.nix
@@ -34,33 +34,8 @@ in {
       description = "The path to the file containing the app key secret";
     };
 
-    tls = {
-      enable = mkEnableOption ''
-        Whether to enable TLS.
-
-        If this option is set to true and tls.cert-private-key or tls.cert-file are null, a signed certifiacate will be requested using ACME. If the proper networking/DNS are not setup a self-signed certificate will be used instead.
-      '';
-
-      cert-key = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Path to the TLS certificate private key file";
-      };
-
-      cert-file = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Path to the TLS certificate file";
-      };
-
-      email = mkOption {
-        type = types.str;
-        default = "someone@somedomain.com";
-        description = ''
-          The email to use for automatic SSL certificates
-          This email will also get SSL certificate renewal email notifications
-        '';
-      };
+    tls = import ../../_shared/nginx-tls.nix {
+      inherit lib;
     };
   };
 
