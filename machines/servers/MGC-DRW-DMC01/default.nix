86c54b4be407b8617c6eb935cce94f8e7f02870c
Ben Harris <ben.r.harris@protonmail.com>
2025-06-21 16:44:17 +0930

    adding semaphore dns name

--- machines/servers/MGC-DRW-DMC01/default.nix
+++ machines/servers/MGC-DRW-DMC01/default.nix
@@ -54,17 +54,18 @@ in
               enable = true;
               domain = vars.networking.internalDomain;
               hosts = ''
-                ${vars.networking.hostsAddr.MGC-DRW-DGW01.eth.ipv4} MGC-DRW-DGW01
+                ${vars.networking.hostsAddr.MGC-DRW-BKS01.eth.ipv4} MGC-DRW-BKS01
                 ${vars.networking.hostsAddr.MGC-DRW-BST01.eth.ipv4} MGC-DRW-BST01
-                ${vars.networking.hostsAddr.MGC-DRW-RST01.eth.ipv4} MGC-DRW-RST01
-                ${vars.networking.hostsAddr.MGC-DRW-RVP01.eth.ipv4} MGC-DRW-RVP01
+                ${vars.networking.hostsAddr.MGC-DRW-DMC01.eth.ipv4} MGC-DRW-DMC01
+                ${vars.networking.hostsAddr.MGC-DRW-DGW01.eth.ipv4} MGC-DRW-DGW01
+                ${vars.networking.hostsAddr.MGC-DRW-FBR01.eth.ipv4} MGC-DRW-FBR01
                 ${vars.networking.hostsAddr.MGC-DRW-HVS01.eth.ipv4} MGC-DRW-HVS01
                 ${vars.networking.hostsAddr.MGC-DRW-HVS02.eth.ipv4} MGC-DRW-HVS02
                 ${vars.networking.hostsAddr.MGC-DRW-HVS03.eth.ipv4} MGC-DRW-HVS03
-                ${vars.networking.hostsAddr.MGC-DRW-DMC01.eth.ipv4} MGC-DRW-DMC01
-                ${vars.networking.hostsAddr.MGC-DRW-FBR01.eth.ipv4} MGC-DRW-FBR01
+                ${vars.networking.hostsAddr.MGC-DRW-RST01.eth.ipv4} MGC-DRW-RST01
+                ${vars.networking.hostsAddr.MGC-DRW-RVP01.eth.ipv4} MGC-DRW-RVP01
+                ${vars.networking.hostsAddr.MGC-DRW-SEM01.eth.ipv4} MGC-DRW-SEM01
                 ${vars.networking.hostsAddr.MGC-DRW-VLT01.eth.ipv4} MGC-DRW-VLT01
-                ${vars.networking.hostsAddr.MGC-DRW-BKS01.eth.ipv4} MGC-DRW-BKS01
                 192.168.1.99 MGC-DRW-FRW01
                 ${vars.networking.hostsAddr.MGC-DRW-BKS01.eth.ipv4} bookstack.${vars.networking.internalDomain}
                 ${vars.networking.hostsAddr.MGC-DRW-FBR01.eth.ipv4} file-browser.${vars.networking.internalDomain}
