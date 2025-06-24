bd0f90b977458a3ccd474e019c865c8bfd0091e6
Ben Harris <ben.r.harris@protonmail.com>
2025-06-24 15:14:39 +0930

    adding dmc02

---
+++ machines/servers/MGC-DRW-DMC02/default.nix
@@ -0,0 +1,48 @@
+{
+  nixpkgs,
+  vars,
+  self,
+  ...
+}:
+
+nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.default
+      {
+        imports = [
+          ../../_shared/qemu-hardware-config.nix
+          (import ../../_shared/common-config.nix {
+            inherit vars;
+          })
+        ];
+
+        networking.hostName = "MGC-DRW-DMC01";
+
+        system.stateVersion = "25.05";
+
+        megacorp = {
+          config = {
+            bootloader.enable = true;
+
+            networking.static-ip = {
+              enable = true;
+              ipv4 = vars.networking.hostsAddr.MGC-DRW-DMC01.eth.ipv4;
+              interface = vars.networking.hostsAddr.MGC-DRW-DMC01.eth.name;
+              gateway = vars.networking.defaultGateway;
+              lan-domain = vars.networking.internalDomain;
+            };
+
+            openssh = {
+              enable = true;
+              authorized-ssh-keys = vars.keys.bastionPubKey;
+            };
+          };
+
+          services = {
+          };
+
+          virtualisation.libvirt.guest.enable = true;
+        };
+      }
+    ];
+  }
