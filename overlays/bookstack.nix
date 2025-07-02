e7a86793b41d5cf0fd14291b5858b08bfd2cbe02
Ben Harris <ben.r.harris@protonmail.com>
2025-07-02 20:02:16 +0930

    fixing typo

--- overlays/bookstack.nix
+++ overlays/bookstack.nix
@@ -1,4 +1,4 @@
-# bookstack-setup.service in 24.05 errors out with "doctrine deprecation not found"
+# bookstack-setup.service in 25.05 errors out with "doctrine deprecation not found"
 # Here we use the 24.11 version of bookstack instead
 {nixpkgs24-11}: final: prev: {
   bookstack = nixpkgs24-11.legacyPackages.${final.system}.bookstack;
