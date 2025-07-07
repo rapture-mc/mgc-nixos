86c8d0a75fd69bdd44a8e5570060206e4c96e396
Ben Harris <ben.r.harris@protonmail.com>
2025-07-07 11:56:32 +0930

    re-adding running

--- modules/virtualisation/libvirt/infra.nix
+++ modules/virtualisation/libvirt/infra.nix
@@ -93,6 +93,11 @@ in {
                 default = true;
               };
 
+              running = mkOption {
+                type = types.bool;
+                default = true;
+              };
+
               memory = mkOption {
                 type = types.str;
                 default = "4096";
