40c65a0c4f3f96ab7fa934b7c0b96c72653be454
Ben Harris <ben.r.harris@protonmail.com>
2025-07-09 21:10:13 +0930

    re-adding TMS01

--- machines/hypervisors/MGC-DRW-HVS01/default.nix
+++ machines/hypervisors/MGC-DRW-HVS01/default.nix
@@ -70,13 +70,13 @@ nixpkgs.lib.nixosSystem {
               vcpu = 2;
             };
 
-            # terminal-servers = {
-            #   vm_hostname_prefix = "MGC-DRW-TMS";
-            #   os_img_url = "/var/lib/libvirt/images/packer-win2022.qcow2";
-            #   memory = "8192";
-            #   vcpu = 3;
-            #   autostart = false;
-            # };
+            terminal-servers = {
+              vm_hostname_prefix = "MGC-DRW-TMS";
+              os_img_url = "/var/lib/libvirt/images/packer-win2022.qcow2";
+              memory = "8192";
+              vcpu = 3;
+              autostart = false;
+            };
 
             bookstack-servers = {
               vm_hostname_prefix = "MGC-DRW-BKS";
