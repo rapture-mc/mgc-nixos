92588f719b313dfdf1cc035e541b64f15b45beb2
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-07-13 17:40:04 +0930

    not needed

--- vars/default.nix
+++ vars/default.nix
@@ -4,6 +4,4 @@
   domains = import ./domains.nix;
 
   adminUser = "benny";
-
-  terraformModuleSource = "git::https://github.com/rapture-mc/terraform-libvirt-module.git?ref=40acff807a0ffb1c0da741774c37ebeda90730b7";
 }
