8ec997bae9de6ffb449b09026513b7c91b342da5
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-07-20 14:15:46 +0930

    removing john's sudo access

--- vars/users.nix
+++ vars/users.nix
@@ -20,7 +20,6 @@
   };
 
   "john.smith" = {
-    sudo = true;
     authorized-ssh-keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
     ];
