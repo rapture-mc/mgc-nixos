c6c26f4121be8c09b00e5b984fcc55f3dad5f0a7
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-07-20 14:03:20 +0930

    removing kristins sudo access

--- vars/users.nix
+++ vars/users.nix
@@ -14,7 +14,6 @@
   };
 
   "kristin.cabot" = {
-    sudo = true;
     authorized-ssh-keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
     ];
