17be1746653a08d0d55db86db8e5f284b96eb39a
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-07-20 13:58:09 +0930

    adding more users

--- vars/users.nix
+++ vars/users.nix
@@ -12,4 +12,18 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
     ];
   };
+
+  "kristin.cabot" = {
+    sudo = true;
+    authorized-ssh-keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
+    ];
+  };
+
+  "john.smith" = {
+    sudo = true;
+    authorized-ssh-keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
+    ];
+  };
 }
