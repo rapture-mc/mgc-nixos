bcae5a60261a634d3f145a392690f7462725c69e
Ben Harris <ben.harris@mgc-drw-bst01>
2025-07-21 11:27:30 +0930

    removing desktop and joining to domain

--- machines/servers/MGC-DRW-VLT01/default.nix
+++ machines/servers/MGC-DRW-VLT01/default.nix
@@ -31,11 +31,6 @@ nixpkgs.lib.nixosSystem {
         config = {
           bootloader.enable = true;
 
-          desktop = {
-            xrdp = true;
-            enable = true;
-          };
-
           networking.static-ip = {
             enable = true;
             ipv4 = vars.networking.hostsAddr.MGC-DRW-VLT01.eth.ipv4;
@@ -43,6 +38,12 @@ nixpkgs.lib.nixosSystem {
             gateway = vars.networking.defaultGateway;
             nameservers = vars.networking.nameServers;
             lan-domain = vars.domains.internalDomain;
+          };
+
+          system.ad-domain = {
+            enable = true;
+            domain-name = vars.domains.internalDomain;
+            netbios-name = "PROD";
           };
         };
 
@@ -53,39 +54,49 @@ nixpkgs.lib.nixosSystem {
             logo = true;
             open-firewall = true;
             address = "vault.${vars.domains.internalDomain}";
+
             tls = {
               enable = true;
               cert-file = "/var/lib/nginx/vault.crt";
               cert-key = "/var/lib/nginx/vault.pem";
             };
+
             pki = {
               enable = true;
               terraform = {
                 action = "plan";
                 state-dir = "/var/lib/terranix-state/vault";
               };
+
               certs = {
                 bookstack = {
                   common_name = "bookstack.${vars.domains.internalDomain}";
                 };
+
                 grafana = {
                   common_name = "grafana.${vars.domains.internalDomain}";
                 };
+
                 mgc-drw-frw01 = {
                   common_name = "mgc-drw-frw01.${vars.domains.internalDomain}";
                 };
+
                 mgc-drw-dmc01 = {
                   common_name = "mgc-drw-dmc01.${vars.domains.internalDomain}";
                 };
+
                 semaphore = {
                   common_name = "semaphore.${vars.domains.internalDomain}";
                 };
+
                 snipe-it = {
                   common_name = "snipe-it.${vars.domains.internalDomain}";
                 };
+
                 vault = {
                   common_name = "vault.${vars.domains.internalDomain}";
                 };
+
                 zabbix = {
                   common_name = "zabbix.${vars.domains.internalDomain}";
                 };
