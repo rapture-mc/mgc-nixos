2d92b868ea2d1978ffb1b251c163cff9de6ad2a0
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-05 11:25:09 +0930

    same as last commit

--- machines/servers/MGC-DRW-K3S01/default.nix
+++ machines/servers/MGC-DRW-K3S01/default.nix
@@ -42,6 +42,8 @@ nixpkgs.lib.nixosSystem {
             domain-name = vars.domains.internalDomain;
             netbios-name = "PROD";
             local-auth = {
+              login = false;
+              sudo = false;
               sshd = false;
               xrdp = false;
             };
