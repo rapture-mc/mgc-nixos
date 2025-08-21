5bb32650782b5c1157f90f3bbbb356b354cf20dd
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-21 16:23:10 +0930

    adding from_port and to_port

--- modules/cloud/aws/ec2.nix
+++ modules/cloud/aws/ec2.nix
@@ -112,6 +112,8 @@
             egress = [
               {
                 description = "Allow all traffic out";
+                from_port = 0;
+                to_port = 0;
                 protocol = "-1";
                 cidr_blocks = ["0.0.0.0/0"];
                 ipv6_cidr_blocks = [];
