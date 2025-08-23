1420458278f8909f2b27ae206d7562512aa9190e
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-23 11:04:47 +0930

    adding amazon-image module

--- machines/servers/MGC-DRW-MBX01/default.nix
+++ machines/servers/MGC-DRW-MBX01/default.nix
@@ -7,8 +7,9 @@
 nixpkgs.lib.nixosSystem {
   modules = [
     self.nixosModules.default
-    {
+    ({modulesPath, ...}: {
       imports = [
+        "${modulesPath}/virtualisation/amazon-image.nix"
         (import ../../_shared/common-config.nix {
           inherit vars;
         })
@@ -19,6 +20,6 @@ nixpkgs.lib.nixosSystem {
       system.stateVersion = "25.05";
 
       nixpkgs.hostPlatform = nixpkgs.lib.mkDefault "x86_64-linux";
-    }
+    })
   ];
 }
