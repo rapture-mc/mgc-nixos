120c35f30dbbf1860fe225637545c5974dbed1dc
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-24 10:10:10 +0930

    enabling headscale

--- machines/servers/MGC-APSE2-HDS01/default.nix
+++ machines/servers/MGC-APSE2-HDS01/default.nix
@@ -20,6 +20,10 @@ nixpkgs.lib.nixosSystem {
       system.stateVersion = "25.05";
 
       nixpkgs.hostPlatform = nixpkgs.lib.mkDefault "x86_64-linux";
+
+      services.headscale = {
+        enable = true;
+      };
     })
   ];
 }
