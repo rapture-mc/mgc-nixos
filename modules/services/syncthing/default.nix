4998c2419140c1e5b46a28352a10c86d80bd770b
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-25 15:38:50 +0930

    testing

--- modules/services/syncthing/default.nix
+++ modules/services/syncthing/default.nix
@@ -130,8 +130,8 @@ in {
           if cfg.gui.enable
           then "0.0.0.0:8384"
           else "127.0.0.1:8384";
-        dataDir = "/home/${cfg.user}/Documents";
-        configDir = "/home/${cfg.user}/.config/syncthing";
+        # dataDir = "/home/${cfg.user}/Documents";
+        # configDir = "/home/${cfg.user}/.config/syncthing";
         overrideDevices = true;
         overrideFolders = true;
         settings = {
