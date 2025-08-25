e1d24da1e7ac7453a88dd651f018794b058f7e39
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-25 16:18:46 +0930

    changing sycnthing dir

--- modules/services/syncthing/default.nix
+++ modules/services/syncthing/default.nix
@@ -146,7 +146,7 @@ in {
           then "0.0.0.0:8384"
           else "127.0.0.1:8384";
         # dataDir = "/home/${cfg.user}/Documents";
-        # configDir = "/home/${cfg.user}/.config/syncthing";
+        configDir = "/etc/syncthing";
         overrideDevices = true;
         overrideFolders = true;
         settings = {
