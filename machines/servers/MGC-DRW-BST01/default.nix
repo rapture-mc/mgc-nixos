a1bf8e58057ffe150dd9f694e0a0191646f81509
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-25 21:28:34 +0930

    adding LT01 to HDS01 devices

--- machines/servers/MGC-DRW-BST01/default.nix
+++ machines/servers/MGC-DRW-BST01/default.nix
@@ -45,12 +45,14 @@ nixpkgs.lib.nixosSystem {
             gui = true;
             devices = {
               MGC-APSE2-HDS01.id = vars.syncthing.MGC-APSE2-HDS01.id;
+              MGC-LT01.id = vars.syncthing.MGC-LT01.id;
             };
             folders = {
               sync = {
                 path = "/home/ben.harris/Sync";
                 devices = [
                   "MGC-APSE2-HDS01"
+                  "MGC-LT01"
                 ];
               };
             };
