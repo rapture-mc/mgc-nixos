99422ba86b28dc87c0892fc494bd042f6d347999
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-26 13:54:40 +0930

    try this

--- machines/servers/MGC-DRW-BST01/secrets.nix
+++ machines/servers/MGC-DRW-BST01/secrets.nix
@@ -5,7 +5,10 @@
     age.keyFile = "/home/${vars.adminUser}/.config/sops/age/keys.txt";
     secrets = {
       restic-repo-password = {};
-      syncthing-admin-password.owner = "ben.harris";
+      syncthing-admin-password = {
+        owner = "ben.harris";
+        sopsFile = ../../../sops/syncthing.yml;
+      };
     };
   };
 }
