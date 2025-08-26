37f5e940fb1aa36a535f23ac52c8a26e32383f95
Ben Harris <ben.harris@mgc-drw-bst01>
2025-08-26 13:56:15 +0930

    it works

--- machines/servers/MGC-APSE2-HDS01/secrets.nix
+++ machines/servers/MGC-APSE2-HDS01/secrets.nix
@@ -4,7 +4,10 @@
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/ben.harris/.config/sops/age/keys.txt";
     secrets = {
-      syncthing-admin-password.owner = "ben.harris";
+      syncthing-admin-password = {
+        owner = "ben.harris";
+        sopsFile = ../../../sops/syncthing.yml;
+      };
     };
   };
 }
