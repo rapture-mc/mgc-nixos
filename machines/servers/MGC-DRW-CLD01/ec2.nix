3781e2e6884b7bf778c5bbefddc47fc391f8fde6
rapture-mc <145347290+rapture-mc@users.noreply.github.com>
2025-08-29 11:28:38 +0930

    Update ec2.nix

--- machines/servers/MGC-DRW-CLD01/ec2.nix
+++ machines/servers/MGC-DRW-CLD01/ec2.nix
@@ -44,6 +44,11 @@
         root_block_device.size = 100;
       };
 
+      veldin = {
+        instance_type = "t2.medium";
+        create_eip = true;
+        root_block_device.size = 100;
+      };
     };
   };
 }
