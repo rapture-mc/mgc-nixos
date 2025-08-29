71d3d664f72e0d634a86a4e7d78a47fdf192fcfc
rapture-mc <145347290+rapture-mc@users.noreply.github.com>
2025-08-29 12:13:45 +0930

    Update default.nix

--- machines/aws/veldin/default.nix
+++ machines/aws/veldin/default.nix
@@ -20,6 +20,11 @@ nixpkgs.lib.nixosSystem {
       system.stateVersion = "25.05";
 
       nixpkgs.hostPlatform = nixpkgs.lib.mkDefault "x86_64-linux";
+
+      megacorp.config.openssh = {
+        bastion-logo = true;
+        allow-password-auth = true;
+      };
     })
   ];
 }
