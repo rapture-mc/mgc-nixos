71fdab035795d3dcd155f6d89d76b7483bd06bf3
rapture-mc <145347290+rapture-mc@users.noreply.github.com>
2025-08-30 10:01:11 +0930

    Update users.nix

--- vars/users.nix
+++ vars/users.nix
@@ -11,6 +11,7 @@
     authorized-ssh-keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzlYmoWjZYFeCNdMBCHBXmqpzK1IBmRiB3hNlsgEtre benny@MGC-DRW-BST01"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4cRviP5Fs5YX/WZLAl3ln8tws9cDXTknA5zJHu3hG ben_harris@MGC-DRW-HVS02"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLRrtrE0Mjx5IkadwVa/zni36PQuG1QM8uQWDAoWAUR ben_harris@MGC-LT02"
     ];
   };
 }
